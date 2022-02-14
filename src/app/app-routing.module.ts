3bfa88cac984caf3dcd8364440660005294126f9
Scarnface <scarnface@tutanota.com>
2022-02-14 15:09:33 +0000

    Home page completed.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from "./home/home.component";
+import { CreateComponent } from "./create/create.component";
+import { BattleComponent } from "./battle/battle.component";
+
+const routes: Routes = [
+  { path: '', component:HomeComponent },
+  { path: 'create', component:CreateComponent },
+  { path: 'battle', component:BattleComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CreateComponent } from './create/create.component';
+import { BattleComponent } from './battle/battle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CreateComponent,
+    BattleComponent
   ],
   imports: [
     BrowserModule,
