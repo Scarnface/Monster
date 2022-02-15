1e6f55b20db692a9360aa02864b32bd950edb4fe
Scarnface <scarnface@tutanota.com>
2022-02-15 09:40:02 +0000

    Routing updated additional reveal page added. Create page styling completed.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from "./home/home.component";
 import { CreateComponent } from "./create/create.component";
+import { RevealComponent } from "./reveal/reveal.component";
 import { BattleComponent } from "./battle/battle.component";
 
 const routes: Routes = [
   { path: '', component:HomeComponent },
   { path: 'create', component:CreateComponent },
+  { path: 'reveal', component:RevealComponent },
   { path: 'battle', component:BattleComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CreateComponent } from './create/create.component';
 import { BattleComponent } from './battle/battle.component';
+import { RevealComponent } from './reveal/reveal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CreateComponent,
-    BattleComponent
+    BattleComponent,
+    RevealComponent
   ],
   imports: [
     BrowserModule,
