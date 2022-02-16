4cbad0d77c6d4f5765d35f45171a00b748e8f0ec
Scarnface <scarnface@tutanota.com>
2022-02-16 09:21:07 +0000

    Added monster fill type generation.

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -14,9 +14,14 @@ export class CreateComponent implements OnInit {
   }
 
   revealMonster(seed:string) {
-    if(seed.length > 10) {
-      seed = seed.substring(0, 10);
+    // The first character of a barcode is a checking digit. We remove this.
+    seed = seed.slice(1);
+    // Ensure the remaining code does not exceed the value accepted by the API.
+    if(seed.length > 9) {
+      // If it does create a new seed of last nine characters.
+      seed = seed.slice(seed.length - 9);
     }
+    // Navigate to next page with seed.
     return this.router.navigate(['/reveal', {seed: seed}]);
   }
 }
--- src/app/reveal/reveal.component.ts
+++ src/app/reveal/reveal.component.ts
@@ -8,11 +8,23 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class RevealComponent implements OnInit {
 
+  fill = 0
+  seed = this.route.snapshot.params['seed']
   monsterUrl = "https://app.pixelencounter.com/api/basic/svgmonsters/";
 
   constructor(private route : ActivatedRoute) { }
 
   ngOnInit() {
-    this.monsterUrl += this.route.snapshot.params['seed'];
+    this.genMonsterStats();
+    this.monsterUrl += (this.seed + "?fillType=" + this.fill);
+  }
+
+  genMonsterStats() {
+    // Generate the monsters fill type from the first character
+    if(this.seed.slice(0, 1) == 0) {
+      this.fill = 5;
+    } else {
+      this.fill = Math.floor(this.seed.slice(0, 1) / 2);
+    }
   }
 }
