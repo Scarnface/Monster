69373589c11fcebad174f421d63c0e8a4b322234
Scarnface <scarnface@tutanota.com>
2022-02-18 10:41:41 +0000

    Combat screen updated. Enemies added.

--- src/app/battle/battle.component.ts
+++ src/app/battle/battle.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BattleComponent implements OnInit {
 
+  turn = false
+  round = 1
+  knockedOut = false
+
+  name: string = 'placeholder';
+  attack = 0
+  defense = 0
+  initiative = 0
+  cpu: any = {};
+
+  enemies = {
+    first: {
+      name: "Bokrug",
+      attack: 10,
+      defense: 20,
+      initiative: 20,
+      url: 'https://app.pixelencounter.com/api/basic/svgmonsters/4356729?fillType=2',
+    },
+    second: {
+      name: "Thog",
+      attack: 20,
+      defense: 40,
+      initiative: 40,
+      url: 'https://app.pixelencounter.com/api/basic/svgmonsters/2223129?fillType=4',
+    },
+    third: {
+      name: "Cthulhu",
+      attack: 40,
+      defense: 80,
+      initiative: 80,
+      url: 'https://app.pixelencounter.com/api/basic/svgmonsters/55515591?fillType=5',
+    }
+  }
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    if(this.round === 1) {
+      this.cpu = this.enemies.first;
+      console.log(this.cpu);
+    }
   }
 
 }
